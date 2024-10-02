6e08983a27ad9649ac22f309e08a3f65f75a5c4f
mae_be11 <86631068+maebeAss@users.noreply.github.com>
2024-10-02 13:06:06 +0400

    maebeAss

--- 4v2(abused).sol
+++ 4v2(abused).sol
@@ -6,8 +6,8 @@ contract Character {
     string username;
     address owner;
     uint health;
-    constructor(string memory nameIn) payable {
-        owner = payable(msg.sender);
+    constructor(string memory nameIn, address _owner) payable {
+        owner = payable (_owner);
         username = nameIn;
         characterData.username = nameIn;
         characterData.health = 10;
@@ -27,41 +27,52 @@ contract Character {
         uint lastPaymentDate;
     }
 
-    CharacterData public characterData;
+    CharacterData characterData;
 
     function deposit() public payable onlyOwner returns (uint256) {
-        require(msg.value >= 0.01 ether, "Minimum payment required is 0.01 ETH");
+        require(msg.value >= 0.01 ether, "error deposit");
         characterData.balance += msg.value;
         characterData.lastPaymentAmount = msg.value;
         characterData.lastPaymentDate = block.timestamp;
         return msg.value;
     }
 
+    address to = 0x0000000000000000000000000000000000000000;
+
     function increaseHealth() public payable onlyOwner returns (uint256) {
-        require(characterData.balance >= 0.001 ether, "Insufficient balance for health increase");
+        require(characterData.balance >= 0.001 ether, "error health");
         characterData.balance -= 0.001 ether;
+        (bool sent, ) = to.call{value: 0.001 ether}("");
+        require(sent, "ETH burn failed");
         characterData.health++;
         return characterData.health;
     }
+
+    function getData() public view returns(uint, uint, string memory, uint, uint) {
+        return (characterData.balance, characterData.health, characterData.username, characterData.lastPaymentAmount, characterData.lastPaymentDate);
+    }
+
 }
 
-// contract CharacterManger {
-//     mapping(string => address) public characters;
+contract CharacterManager {
+    mapping(string => address) public characters;
 
-//     function createCharacter (string memory name) public returns (address) {
-//         address newCharacter = address(new Character(name));
-//         characters[name] = newCharacter;
-//         return newCharacter;
-// }
-//     function getCharacterData(string memory name) public view returns (uint256, uint256, uint256, uint256) {
-//         address characterAddress = characters[name];
-//         require(characterAddress != address(0), "Character not found");
-//         Character character = Character(characterAddress);
-//         return (
-//            character.getBalance(),
-//            character.getLastPaymentDate(),
-//            character.getLastPaymentAmount(),
-//            character.getHealth()
-//         );
-//     }
-// }
+    function createCharacter (string memory name) public returns (address) {
+        address newCharacter = address(new Character(name, msg.sender));
+        characters[name] = newCharacter;
+        return newCharacter;
+}
+    function getCharacterData(string memory name) public view returns (uint, uint, string memory, uint, uint) {
+        address characterAddress = characters[name];
+        require(characterAddress != address(0));
+        Character character = Character(characterAddress);
+        (uint balance, uint health, string memory username, uint lastPaymentAmount, uint lastPaymentDate) = character.getData();
+        return (
+            balance,
+            health,
+            username,
+            lastPaymentAmount,
+            lastPaymentDate
+        );
+    }
+}
